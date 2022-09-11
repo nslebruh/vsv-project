92e81f4fcfb05d0121b4b8556012f3c72970dbba
nslebruh <nslebrun1@gmail.com>
2022-09-11 20:21:14 +1000

    I'm the big dum dum

--- src/firebase/functions/auth.tsx
+++ src/firebase/functions/auth.tsx
@@ -1,5 +1,7 @@
 import { firebaseAuth as auth } from "../context/auth";
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword, UserCredential } from "firebase/auth"
+import { getRef } from "./database";
+import { DataSnapshot, get, set } from "firebase/database";
 
 
 export const signUpUser = (email: string, password: string) => {
@@ -8,6 +10,20 @@ export const signUpUser = (email: string, password: string) => {
       // Sign up successful. 
       const user = userCredential.user
       console.log(user)
+      const newUser = {
+        name: user.displayName || user.email,
+        channels: {
+          "root": true
+        }
+      }
+      const reference = getRef(`/users/${user.uid}`)
+      set(reference, newUser)
+        .then((value) => {
+          console.log(value)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
     })
     .catch((error) => {
       const errorCode = error.code;
@@ -20,7 +36,23 @@ export const signInUser = (email: string, password: string) => {
     .then((userCredential) => {
       // Sign in sucessful.
       const user = userCredential.user
+      const checkForRootChannel = () => {
+        const reference = getRef(`/users/${user.uid}/channels`)
+        get(reference)
+          .then((value: DataSnapshot) => {
+            if (!value.hasChild("root")) {
+              set(reference, {"root": true})
+                .then((val) => {
+                  console.log(val)
+                })
+                .catch((err) => {
+                  console.log(err)
+                })
+            }
+          })
+      }
       console.log("user:", user);
+      checkForRootChannel()
     })
     .catch((error) => {
       const errorCode = error.code;
